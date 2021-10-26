63d22dcb909b774d6b5e50ce47fc6c500ddf5416
CJBiohacker <cj.moyses@gmail.com>
2021-10-25 22:43:16 -0300

    "Declaração e Inicialização de variável"

---
+++ Programacao-Imperativa/ex2.js
@@ -0,0 +1,7 @@
+let nome = "Carlos";
+let idade = 31;
+let statusUsuario = true;
+
+console.log(`Nome: ${nome}`);
+console.log(`Idade: ${idade}`);
+console.log(`Status do Usuário: ${statusUsuario}`);
