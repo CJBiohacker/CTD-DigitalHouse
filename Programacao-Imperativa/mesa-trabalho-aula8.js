87aa78e50dc9a64a607b9813a8e700e4a1f222e9
CJBiohacker <cj.moyses@gmail.com>
2021-11-08 21:46:59 -0300

    "Fluxo de Aplicação e Condicional"

---
+++ Programacao-Imperativa/mesa-trabalho-aula8.js
@@ -0,0 +1,18 @@
+function podeSubir(altura, companhia) {
+    let passagem;
+
+    if (altura < 1.20 ) {
+        passagem = false;
+        return console.log('Acesso negado');
+    } else if (altura < 1.40 && companhia === true) {
+        passagem = true;
+        return console.log('Acesso autorizado somente com acompanhante.');
+    } else if (altura > 1.40 && altura < 2) {
+        passagem = true;
+        return console.log('Acesso autorizado');
+    } else {
+        return console.log('Acesso negado');
+    }
+};
+
+podeSubir(1.10,true);
