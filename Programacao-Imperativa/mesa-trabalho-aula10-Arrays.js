e5d2000843576120ba906da192b961bd61c2657d
CJBiohacker <cj.moyses@gmail.com>
2021-11-16 23:35:58 -0300

    "Mesa de Trabalho Aula 10"

--- Programacao-Imperativa/mesa-trabalho-aula10-Arrays.js
+++ Programacao-Imperativa/mesa-trabalho-aula10-Arrays.js
@@ -66,3 +66,18 @@ inverter(schoolsWitcher);
 
 // Soma Array
 
+const somaArray = (vetor) => {
+    let soma = 0;
+
+    for (let i = 0; i < vetor.length; i++) {
+        soma = soma + vetor[i];
+    };
+
+    return console.log(soma);
+
+}
+
+somaArray(numbers);
+
+
+
