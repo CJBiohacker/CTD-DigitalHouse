da9759d127d1ea772a5b103f7589c94c17df4261
CJBiohacker <cj.moyses@gmail.com>
2022-02-21 22:34:56 -0300

    Criação da pasta FrontEnd II e explicação da mesa

---
+++ Front End II/pratica_integradora_Aula1.js
@@ -0,0 +1,13 @@
+const array = [1, 2, 4, 8];
+
+console.log("O comprimento da Array é de: " + array.length);
+
+for (let i = 0; i < array.length; i++) {
+    // montar uma lógica que eu diga pro JavaScript que ele some o elemento atual da array com o próximo, ou o atual com o anterior. 
+
+    console.log(array);
+
+}
+
+// 1ª Iteração i = 0 , enquanto (i = 0) < 4
+// 
