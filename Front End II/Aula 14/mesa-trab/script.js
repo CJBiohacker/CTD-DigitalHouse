f8d212a7b8364bf4c46fb956b56fd19dad8f3d3e
CJBiohacker <cj.moyses@gmail.com>
2022-03-23 22:50:14 -0300

    feat : mesa trabalho json e forms

---
+++ Front End II/Aula 14/mesa-trab/script.js
@@ -0,0 +1,12 @@
+let formulario = document.getElementById('form');
+let textoSaida = document.getElementById('comentario-saida');
+
+const capturaComentario = (comentario) => {
+
+    localStorage.getItem(evento);
+    comentario.preventDefault;
+
+};
+
+formulario.addEventListener('submit', capturaComentario());
+
