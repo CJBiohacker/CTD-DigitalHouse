0115ba55580825b56f5fae069695ee8859cb0dd5
Carlos de Lima Junior <carlos.junior@zooxsmart.com>
2023-05-23 17:55:41 -0300

    chore: remove unused useState

--- FrontEnd III/Aula-7/mesa-trab-7/src/App.jsx
+++ FrontEnd III/Aula-7/mesa-trab-7/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import List from './components/List';
 import './App.css';
 
 
 function App() {
-  const [cards, setCards] = useState([]);
-
   return (
     <>
       <h1>Lista de Biomas do Valheim</h1>
